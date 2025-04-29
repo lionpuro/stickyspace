fc44ea1c0284b3d6b039812e6198aeefb3c71315
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-04-29 15:36:56 +0300

    add auth middleware

--- auth/auth.go
+++ auth/auth.go
@@ -3,6 +3,8 @@ package auth
 import (
 	"context"
 	"errors"
+	"net/http"
+	"strings"
 
 	"firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/auth"
@@ -48,6 +50,56 @@ func (s *Service) VerifyToken(ctx context.Context, token string) (*auth.Token, e
 		return nil, err
 	}
 	return tkn, nil
+}
+
+func (s *Service) Middleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ah := r.Header.Get("Authorization")
+		if ah == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		t := strings.Split(ah, " ")
+		if len(t) != 2 || strings.ToLower(t[0]) != "bearer" {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		tkn := t[1]
+
+		token, err := s.VerifyToken(r.Context(), tkn)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		email, ok := token.Claims["email"].(string)
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		emailVerif, ok := token.Claims["email_verified"].(bool)
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		user := User{
+			ID:            token.UID,
+			Email:         email,
+			EmailVerified: emailVerif,
+		}
+		ctx := CtxWithUser(r.Context(), user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+}
+
+func CtxWithUser(ctx context.Context, user User) context.Context {
+	return context.WithValue(ctx, "user", user)
+}
+
+func UserFromCtx(ctx context.Context) (User, bool) {
+	u, ok := ctx.Value("user").(User)
+	return u, ok
 }
 
 func (s *Service) CreateUser(ctx context.Context, email, password, name string) (*auth.UserRecord, error) {
