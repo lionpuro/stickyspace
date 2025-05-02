3bdc612566bc520298c2ba8e6f50d93acc85e3b7
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-05-02 09:56:39 +0300

    update logo

--- frontend/src/lib/layouts/app.svelte
+++ frontend/src/lib/layouts/app.svelte
@@ -6,9 +6,11 @@
 
 <div class="flex min-h-full flex-col">
 	<header class="flex px-4 py-2">
-		<h1 class="flex text-xl font-semibold tracking-tight">
-			Sticky
-			<span class="text-base-500 ml-0.25">Space</span>
+		<h1 class="text-xl font-semibold tracking-tight">
+			<a href="/" class="flex">
+				<span class="text-base-900">sticky</span>
+				<span class="text-base-500 ml-0.25">space</span>
+			</a>
 		</h1>
 		<button onclick={session.logout} class="ml-auto">Log out</button>
 	</header>
